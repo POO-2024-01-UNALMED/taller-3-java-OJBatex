f84315cd61ae2b5f4dbdf0cd638c9fb44edce3ab
OJBatex <simondr2006@gmail.com>
2024-02-24 14:38:44 -0500

    modificación 2

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -3,7 +3,7 @@ package taller3.televisores;
 public class TV {
 	private Marca marca;
 	int canal = 1;
-	int precio = 500;
+	private int precio = 500;
 	boolean estado;
 	int volumen = 1;
 	Control control;
